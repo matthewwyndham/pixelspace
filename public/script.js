2e18f9f9319d5d9e9c3bdeef6768d5703fa296cd
Matt Wyndham <matthewwyndham@gmail.com>
2018-07-07 02:04:28 -0600

    Auto::Sat 07/07/2018:: 2:04:28.09

--- public/script.js
+++ public/script.js
@@ -8,7 +8,17 @@
       }});
     }
 
-    function create_new(x, y, r, g, b, a) {
+    function create_new() {
+      var x = document.getElementById('form-x');
+      var y = document.getElementById('form-y');
+      var r = document.getElementById('form-r');
+      var g = document.getElementById('form-g');
+      var b = document.getElementById('form-b');
+      var a = document.getElementById('form-a');
+      var n = document.getElementById('form-name');
+      var addurl = "/add?x=" + x.toString() + "&y=" + y.toString() + "&r=" + r.toString() + "&g=" + g.toString() + "&b=" + b.toString() + "&a=" + a.toString() + "&username=" + n.toString();
+      $.ajax({url: addurl, success: function(){
         var identity = "x" + x.toString() + "y" + y.toString();
-          $("#" + identity).css('backgroundColor', 'rgba(' + r.toString() + ', ' + g.toString() + ', ' + b.toString() + ', ' + a.toString() + ')');
+        $("#" + identity).css('backgroundColor', 'rgba(' + r.toString() + ', ' + g.toString() + ', ' + b.toString() + ', ' + a.toString() + ')');  
+      }});
     }
