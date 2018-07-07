d662bdd20baa8157a51d63f858b2621bf4a5bc04
Matt Wyndham <matthewwyndham@gmail.com>
2018-07-07 02:11:15 -0600

    Auto::Sat 07/07/2018:: 2:11:15.59

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
   function refresh() {
       $.ajax({url: "/allPixels", success: function(result) {
-        console.log(result);
+        //console.log(result);
         result.forEach(row => {
           var identity = "x" + (row.x).toString() + "y" + (row.y).toString();
           $("#" + identity).css('backgroundColor', 'rgba(' + (row.r).toString() + ', ' + (row.g).toString() + ', ' + (row.b).toString() + ', ' + (row.a).toString() + ')');
