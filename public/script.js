8edbabf416febe9aea6b51598161dbd36325c244
Matt Wyndham <matthewwyndham@gmail.com>
2018-07-07 02:10:02 -0600

    Auto::Sat 07/07/2018:: 2:10:02.64

--- public/script.js
+++ public/script.js
@@ -9,13 +9,13 @@
     }
 
     function create_new() {
-      var x = document.getElementById('form-x');
-      var y = document.getElementById('form-y');
-      var r = document.getElementById('form-r');
-      var g = document.getElementById('form-g');
-      var b = document.getElementById('form-b');
-      var a = document.getElementById('form-a');
-      var n = document.getElementById('form-name');
+      var x = document.getElementById('form-x').value;
+      var y = document.getElementById('form-y').value;
+      var r = document.getElementById('form-r').value;
+      var g = document.getElementById('form-g').value;
+      var b = document.getElementById('form-b').value;
+      var a = document.getElementById('form-a').value;
+      var n = document.getElementById('form-name').value;
       var addurl = "/add?x=" + x.toString() + "&y=" + y.toString() + "&r=" + r.toString() + "&g=" + g.toString() + "&b=" + b.toString() + "&a=" + a.toString() + "&username=" + n.toString();
       $.ajax({url: addurl, success: function(){
         var identity = "x" + x.toString() + "y" + y.toString();
