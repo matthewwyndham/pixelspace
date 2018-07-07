c2c1bfa9397dcbb8d4f79cb524dc1db2e3ed6044
Matt Wyndham <matthewwyndham@gmail.com>
2018-07-07 00:39:03 -0600

    Auto::Sat 07/07/2018:: 0:39:02.98

--- public/script.js
+++ public/script.js
@@ -2,9 +2,13 @@
       $.ajax({url: "/allPixels", success: function(result) {
         console.log(result);
         result.forEach(row => {
-          var identity = "x" + (row.x.toString()) + "y" + (row.y).toString();
+          var identity = "x" + (row.x).toString() + "y" + (row.y).toString();
           $("#" + identity).css('backgroundColor', 'rgba(' + (row.r).toString() + ', ' + (row.g).toString() + ', ' + (row.b).toString() + ', ' + (row.a).toString() + ')');
-          console.log(row);
         });
       }});
+    }
+
+    function create_new(x, y, r, g, b, a) {
+        var identity = "x" + x.toString() + "y" + y.toString();
+          $("#" + identity).css('backgroundColor', 'rgba(' + r.toString() + ', ' + g.toString() + ', ' + b.toString() + ', ' + a.toString() + ')');
     }
